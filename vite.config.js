fe277e59474631c5afcc4c312c8927f43b6fbcd2
SallyFeng1223 <129814925+SallyFeng1223@users.noreply.github.com>
2026-03-28 10:10:45 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   // ⚠️ 這裡填入您的 GitHub Repository 名稱
-  base: '/joy-english-game/',
+  base: '/cloze-game/',
 })
